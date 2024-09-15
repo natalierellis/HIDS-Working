eb9599e3b0b43a8101b47f3465f08d54a764b4d5
Nate Cobb <nate@drcobb.com>
2024-09-14 22:11:36 -0400

    Class 2 updates and Class 3 prep

---
+++ class_02/01_pipeline.r
@@ -0,0 +1,15 @@
+
+# Raw R
+chicks <- ChickWeight
+chicks <- chicks[nrow(chicks)-10:nrow(chicks), ]
+chicks <- chicks[chicks$weight <= 300, ]
+write.csv(chicks, "class_02/chickweights.csv")
+
+
+# Tidyverse
+library(dplyr)
+ChickWeight %>%
+  tail(nrow(ChickWeight) - 10) %>%
+  filter(weight < 300) %>%
+  write.csv("class_02/chickweights.csv")
+
